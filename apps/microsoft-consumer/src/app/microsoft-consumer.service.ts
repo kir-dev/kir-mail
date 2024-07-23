fc9d8940432964fca5bcd7804fbafb4d9707c74a
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-07-23 23:25:56 +0200

    feat: microsoft provider

--- apps/microsoft-consumer/src/app/microsoft-consumer.service.ts
+++ apps/microsoft-consumer/src/app/microsoft-consumer.service.ts
@@ -1,7 +1,10 @@
 import { SingleSendRequestDto } from '@kir-mail/types';
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Injectable, Logger } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 import { Job, MetricsTime } from 'bullmq';
+
+import { MAIL_USER } from '../config';
 
 const MESSAGES_PER_MONTH = 1_000_000;
 
@@ -21,7 +24,18 @@ const RESET_INTERVAL = 10 * 1000;
 export class MicrosoftConsumerService extends WorkerHost {
   private readonly logger = new Logger(MicrosoftConsumerService.name);
 
+  constructor(private readonly mailerService: MailerService) {
+    super();
+  }
+
   async process(job: Job<SingleSendRequestDto>) {
     this.logger.log(`Processing job: ${job.id} with data: ${JSON.stringify(job.data)}`);
+    await this.mailerService.sendMail({
+      to: job.data.to,
+      from: `"${job.data.from}" <${MAIL_USER}>`,
+      subject: job.data.subject,
+      html: job.data.html,
+    });
+    this.logger.log(`Job ${job.id} processed`);
   }
 }
